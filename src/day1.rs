94325ccd5781558513955a6957c0239874c4e7c4
SteveCookWCU <SC1055963@wcupa.edu>
2024-12-01 00:39:00 -0500

    Day 1

--- src/day1.rs
+++ src/day1.rs
@@ -1,27 +1,53 @@
+use std::collections::HashMap;
+
 pub fn part1(input: &[impl AsRef<str>]) -> String {
-    String::new()
+    let (mut first_list, mut second_list) = parse_lists(input);
+
+    first_list.sort();
+    second_list.sort();
+
+    let mut sum = 0;
+
+    for i in 0..first_list.len() {
+        sum += (first_list[i] - second_list[i]).abs();
+    }
+
+    sum.to_string()
 }
 
 pub fn part2(input: &[impl AsRef<str>]) -> String {
-    String::new()
+    let (left_list, right_list) = parse_lists(input);
+    let mut map = HashMap::new();
+    for first in &left_list {
+        if !map.contains_key(first) {
+            let mut count = 0;
+            for second in &right_list {
+                if first == second {
+                    count += 1;
+                }
+            }
+            map.insert(first, count);
+        }
+    }
+
+    let mut sum = 0;
+
+    for first in &left_list {
+        sum += first * map.get(&first).unwrap();
+    }
+
+    sum.to_string()
 }
 
-#[cfg(test)]
-mod tests {
-    use crate::day1::{part1, part2};
-    use crate::get_test_input;
-
-    static INPUT: &'static str = "";
-
-    #[test]
-    fn test_part1() {
-        let input = get_test_input(INPUT);
-        assert_eq!(part1(&input), "");
+fn parse_lists(input: &[impl AsRef<str>]) -> (Vec<i32>, Vec<i32>) {
+    let mut first_list = Vec::with_capacity(input.len());
+    let mut second_list = Vec::with_capacity(input.len());
+    for line in input.iter().map(AsRef::as_ref) {
+        let (first, second) = line.split_once("   ").unwrap();
+        let first = first.parse::<i32>().unwrap();
+        let second = second.parse::<i32>().unwrap();
+        first_list.push(first);
+        second_list.push(second);
     }
-
-    #[test]
-    fn test_part2() {
-        let input = get_test_input(INPUT);
-        assert_eq!(part2(&input), "");
-    }
+    (first_list, second_list)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 use advent2024_rs::{day1, get_input};
-use std::io::{stdin, BufRead};
+use std::io::{stdin, stdout, BufRead, Write};
 
 fn main() -> anyhow::Result<()> {
     print!("Please input a day: ");
+    stdout().flush()?;
     let mut input = String::new();
     stdin().lock().read_line(&mut input)?;
 
-    let day = input.parse::<u32>()?;
+    let day = input.trim().parse::<u32>()?;
     let lines = get_input(format!("./inputs/day{}.txt", day))?;
 
     let (part1, part2) = match day {
